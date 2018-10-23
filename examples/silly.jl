d8c5e999b6a91ba37259b757f24d0da8f4ab87ef
SebaGraz <43820633+SebaGraz@users.noreply.github.com>
2018-10-23 14:12:22 +0200

    Sample Wiener

--- examples/silly.jl
+++ examples/silly.jl
@@ -1,1 +1,13 @@
-println("Hello world.")
+
+using Bridge
+using Plots
+t= 0:.001:1
+W = sample(t, Wiener())
+plot(W.tt, W.yy)
+struct OU <: Bridge.ContinuousTimeProcess{Float64}
+    μ::Float64
+end
+Bridge.b(s, x, P::OU) = -P.μ*x
+Bridge.σ(s, x, ::OU) = 1.0
+P = OU(1.0)
+Bridge.girsanov(W, P, Wiener())
